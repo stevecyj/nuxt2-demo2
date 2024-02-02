8f8cf88ab9bfe5ce29d343b0f84ea5927c1679a2
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-02 14:43:40 +0800

    init with tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container flex items-center justify-center">
+      <button
+        class="py-2 px-4 bg-green-500 text-white font-semibold rounded-lg shadow-md hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-75"
+      >
+        Click me
+      </button>
+    </div>
+    <Tutorial />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage',
+})
+</script>

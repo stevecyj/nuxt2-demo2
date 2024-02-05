07f925e8261a3f437d50830edfe0f717856baf50
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-05 12:00:02 +0800

    feat: refactor HTML structure and remove unnecessary elements
    - Remove the `ul` element and its content
    - Add a new `div` element with the class `group is-published`
    - Add a new `div` element with the class `hidden group-[.is-published]:block` inside the `group is-published` div
    - Remove the `a` element and its content
    - Remove the `svg` element and its content from the `a` element
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- components/TailWind.vue
+++ components/TailWind.vue
@@ -1,27 +1,8 @@
 <template>
   <div>
-    <ul role="list">
-      {#each people as person}
-      <li class="group/item hover:bg-slate-100 ...">
-        <img src="{person.imageUrl}" alt="" />
-        <div>
-          <a href="{person.url}">{person.name}</a>
-          <p>{person.title}</p>
-        </div>
-        <a
-          class="group/edit invisible hover:bg-slate-200 group-hover/item:visible ..."
-          href="tel:{person.phone}"
-        >
-          <span class="group-hover/edit:text-gray-700 ...">Call</span>
-          <svg
-            class="group-hover/edit:translate-x-0.5 group-hover/edit:text-slate-500 ..."
-          >
-            <!-- ... -->
-          </svg>
-        </a>
-      </li>
-      {/each}
-    </ul>
+    <div class="group is-published">
+      <div class="hidden group-[.is-published]:block">Published</div>
+    </div>
   </div>
 </template>
 

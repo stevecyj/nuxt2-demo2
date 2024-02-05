ecccf567c51efc454f2ec7a7366cb8efcd668452
steve.tsao <steve.tsao@hlwtech.com.tw>
2024-02-05 10:14:02 +0800

    feat: refactor Swiper component usage in pages/index.vue
    - Create a new file `components/SwiperDemo.vue`
    - Add a Swiper component to the template
    - Remove the swiper-wrapper and swiper-pagination divs from the template in `pages/index.vue`
    - Add a SwiperDemo component to the template in `pages/index.vue`
    - Remove the banners and swiperOption data properties from the component in `pages/index.vue`
    - Remove the console.log and slideTo function from the mounted hook in the component in `pages/index.vue`
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ components/SwiperDemo.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div v-swiper:mySwiper="swiperOption">
+    <div class="swiper-wrapper">
+      <div v-for="banner in banners" :key="banner" class="swiper-slide">
+        <img :src="banner">
+      </div>
+    </div>
+    <div class="swiper-pagination"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        banners: [ '/1.jpg', '/2.jpg', '/3.jpg' ],
+        swiperOption: {
+          pagination: {
+            el: '.swiper-pagination'
+          },
+          // ...
+        }
+      }
+    },
+    mounted() {
+      console.log('Current Swiper instance object', this.mySwiper)
+      this.mySwiper.slideTo(3, 1000, false)
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,6 @@
 <template>
-  <div v-swiper:mySwiper="swiperOption">
-    <div class="swiper-wrapper">
-      <div v-for="banner in banners" :key="banner" class="swiper-slide">
-        <img :src="banner">
-      </div>
-    </div>
-    <div class="swiper-pagination"></div>
+  <div>
+    <SwiperDemo />
   </div>
 </template>
 
@@ -13,18 +8,9 @@
   export default {
     data () {
       return {
-        banners: [ '/1.jpg', '/2.jpg', '/3.jpg' ],
-        swiperOption: {
-          pagination: {
-            el: '.swiper-pagination'
-          },
-          // ...
-        }
       }
     },
     mounted() {
-      console.log('Current Swiper instance object', this.mySwiper)
-      this.mySwiper.slideTo(3, 1000, false)
     }
   }
 </script>

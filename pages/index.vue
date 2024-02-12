818cd2834c217a0d9fb04248ad93ca1d12e9e2d0
stevecyj <ycsteven.tsao@gmail.com>
2024-02-12 20:56:51 +0800

    refactor: refactor pagination component and add new properties to generateDataActivity function
    - Changed the variable names in the pagination component to use the `pagi` object instead of separate variables
    - Added a new property `topic` in the `generateDataActivity` function
    - Added new properties `date`, `time`, `title`, and `description` in the `generateDataActivity` function
    - Removed commented out code in the `generateDataActivity` function
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,11 @@
       <div class="h-2 w-100 bg-blue-500"></div>
       <div class="max-w-lg mx-auto my-12">
         <pagination
-          :total-pages="totalPages"
-          :total="total"
-          :per-page="perPage"
-          :current-page="currentPage"
-          :has-more-pages="hasMorePages"
+          :total-pages="pagi.totalPages"
+          :total="pagi.total"
+          :per-page="pagi.perPage"
+          :current-page="pagi.currentPage"
+          :has-more-pages="pagi.hasMorePages"
           @pagechanged="showMore"
         >
         </pagination>
@@ -18,7 +18,7 @@
       <!-- <SwiperDiscoveryActivity :activities="items" /> -->
       <!-- <SwiperPartner :partners="items" /> -->
       <!-- <SwiperNewsHonor :news-honors="items" /> -->
-      <!-- <MixContent /> -->
+      <MixContent />
     </client-only>
   </div>
 </template>
@@ -35,12 +35,14 @@ export default {
   data() {
     return {
       // pagination
-      page: 1,
-      totalPages: 4,
-      total: 40,
-      perPage: 10,
-      currentPage: 1,
-      hasMorePages: true,
+      pagi: {
+        page: 1,
+        totalPages: 4,
+        total: 40,
+        perPage: 10,
+        currentPage: 1,
+        hasMorePages: true,
+      },
       items: [],
     }
   },
@@ -50,8 +52,8 @@ export default {
   },
   methods: {
     showMore(page) {
-      this.page = page
-      this.currentPage = page
+      this.pagi.page = page
+      this.pagi.currentPage = page
     },
   },
 }
--- tools/generateData.js
+++ tools/generateData.js
@@ -29,14 +29,18 @@ export const generateDataActivity = () => {
   for (let i = 0; i < 10; i++) {
     const id = Math.floor(Math.random() * 1000)
     const mediaId = Math.random().toString(36).substring(2, 15)
+    const topics = ['主題講座', '科技英文', '商用基礎文法', '財經英文']
+    const topic = topics[Math.floor(Math.random() * topics.length)]
     items.push({
       id,
       mediaId,
+      topic,
+      date: '2024/01/31 FRI',
+      time: '20:00 PM ~ 22:00 PM',
+      title: '語言共舞：跨足多國文化之旅',
+      description:
+        'platea dictumst vestibulum rhoncus est pellentesque elit ullamcorper dignissim cras tincidunt lobortis feugiat vivamus at',
       // type: 'YOUTUBE',
-      // title:
-      //   '財經英文 08. Advanced Vocabulary for Financial News 8._Monetary Policy',
-      // description:
-      //   '財經英文 8. Advanced Vocabulary for Financial News 8._Monetary Policy',
       // descriptionHtml:
       //   '財經英文 8. Advanced Vocabulary for Financial News 8._Monetary Policy',
       // topic: '財經英文',
